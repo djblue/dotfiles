97b44dca6c9edb2d4700f19e4b762bcb2185f54c
Chris Badahdah <a.s.badahdah@gmail.com>
2015-04-10 23:13:36 -0700

    Added .INSTALL. It maps repos to filesystem locations which are cloned/pulled during the install process. This works for more than just vim plugins, it will fetch arbitrary git repos.

--- install.sh
+++ install.sh
@@ -2,15 +2,32 @@
 # Script to easily install dotfiles.
 
 # script variables
-
-
 cd $(dirname $0)
 dir="$(pwd)"        # dotfiles directory
 olddir="$dir-old"   # old dotfiles backup directory
 files="$(ls $dir)"  # list of files/folders
 
+IFS=$'\n'
+
+install=$(cat .INSTALL)
+
+script_dir=$(dirname `which $0`);
+
+for line in $install; do
+  repo="$(echo $line | cut -f1 -d' ')"
+  dest="$script_dir/$(echo $line | tr -s ' ' | cut -f2 -d' ')"
+  if [ ! -d $dest ]; then
+    echo "cloning $repo to $dest"
+    git clone $repo $dest
+  else
+    echo "pulling $repo to $dest"
+    cd $dest
+    git pull
+  fi
+done
+
 # excludes
-exclude="$0 README.md plugin.sh"
+exclude="$0 README.md"
 
 for file in $files; do
 
@@ -38,7 +55,3 @@ for file in $files; do
     fi
 
 done
-
-# initialize vim plugins
-git submodule init
-git submodule update
--- plugin.sh
+++
@@ -1,9 +0,0 @@
-# script to automate vim plugin installations
-git submodule add https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
-git submodule add https://github.com/altercation/vim-colors-solarized.git vim/bundle/vim-colors-solarized
-git submodule add https://github.com/tpope/vim-fugitive.git vim/bundle/vim-fugitive
-git submodule add https://github.com/jelera/vim-javascript-syntax.git vim/bundle/vim-javascript-syntax
-git submodule add https://github.com/tpope/vim-markdown.git vim/bundle/vim-markdown
-git submodule add https://github.com/Lokaltog/vim-powerline.git vim/bundle/vim-powerline
-git submodule add https://github.com/stephenmckinney/vim-solarized-powerline.git vim/bundle/vim-solarized-powerline
-git submodule add https://github.com/tpope/vim-surround.git vim/bundle/vim-surround
