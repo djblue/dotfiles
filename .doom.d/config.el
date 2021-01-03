25ffcda713a8b5ab5beb80d07bca81de99f20018
Chris Badahdah <a.s.badahdah@gmail.com>
2021-01-03 01:33:49 -0700

    Wait, no, that's what I want for ,q

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -69,7 +69,7 @@
 
 ;; ctrl + p compatibility
 (map! :n "C-p" #'projectile-find-file
-      :n ",q"  #'kill-current-buffer
+      :n ",q"  #'evil-quit
       :n ",c"  #'evil-ex-nohighlight)
 
 (map! :map emacs-lisp-mode-map
